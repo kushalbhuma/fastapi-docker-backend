4fe5a63376eae83262e3b9334321ac81096469f5
kushalbhuma <bhumakushal@example.com>
2026-01-19 11:40:21 +0530

    New Update #1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello FastAPI"}
+    return {"message": "Hello FastAPI,The Server is Running"}
 
 @app.get("/health")
 def health():
